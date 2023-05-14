ece03848bdbcd5148a910594616082f9fc2d4ecc
Ramil Khanov <sa-msk@yandex.ru>
2023-05-15 02:23:20 +0300

    создал плагин сообщений

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -4,6 +4,7 @@ import {PostsService} from "../../shared/posts.service";
 import {Subscription, switchMap} from "rxjs";
 import {Post} from "../../shared/interfaces";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AlertService} from "../shared/services/alert.service";
 
 @Component({
   selector: 'app-edit-page',
@@ -20,7 +21,8 @@ export class EditPageComponent implements OnInit, OnDestroy{
 
   constructor(
     private route: ActivatedRoute,
-    private postsService: PostsService
+    private postsService: PostsService,
+    private alert: AlertService
   ) {
   }
 
@@ -57,6 +59,7 @@ export class EditPageComponent implements OnInit, OnDestroy{
       title: this.form.value.title,
     }).subscribe(() => {
       this.submitted = false
+      this.alert.success('Пост был обновлен')
     })
   }
 
