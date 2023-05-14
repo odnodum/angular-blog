b2435cf05b97b5b65e939de87c738a57549327e6
Ramil Khanov <sa-msk@yandex.ru>
2023-05-15 00:47:31 +0300

    реализовал удаление поста

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -11,6 +11,7 @@ import {Subscription} from "rxjs";
 export class DashboardPageComponent implements OnInit, OnDestroy {
   posts: Post[] = []
   pSub: Subscription
+  dSub: Subscription
   searchStr = ''
 
   constructor(private postsService: PostsService) {
@@ -23,12 +24,18 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
   }
 
   remove(id: string) {
-
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id)
+    })
   }
 
   ngOnDestroy(): void {
     if (this.pSub) {
       this.pSub.unsubscribe()
+    }
+
+    if (this.dSub) {
+      this.dSub.unsubscribe()
     }
   }
 
